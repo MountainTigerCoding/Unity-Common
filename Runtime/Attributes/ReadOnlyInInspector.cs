e9ed5a40cec4be9b515d45065d8a1d7231093e66
Mountain Tiger <134115358+MountainTigerCoding@users.noreply.github.com>
2024-08-02 18:20:42 +1000

    Updated runtime

--- Runtime/Attributes/ReadOnlyInInspector.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Runtime.Shared
-{
-    /// <summary>
-    /// Makes a serializable field unchangedable via the inspector
-    /// </summary>
-    public sealed class ReadOnlyInInspectorAttribute : PropertyAttribute {}
-}
